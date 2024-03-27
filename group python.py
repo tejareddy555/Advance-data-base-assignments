59c8e5da6259d752fbba05c1720c2979f0a86c3c
tejareddy555 <156957867+tejareddy555@users.noreply.github.com>
2024-03-27 13:35:45 -0400

    Add files via upload

---
+++ group python.py
@@ -0,0 +1,32 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import datetime
+
+uri = "mongodb+srv://tejapn42:teja1209@cluster0.vbcpmqk.mongodb.net/"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+try:
+    # Send a ping to confirm a successful connection
+    client.admin.command('ping')
+
+    # Get reference to 'sample_training' database
+    db = client.sample_training
+
+    # Get reference to 'zips' collection
+    accounts_collection = db.zips
+
+    new = [{"$sort":{"pop":-1}},{"$limit":5}]
+
+    result = list(accounts_collection.aggregate(new))
+
+    if result:
+        print(result)
+    else:
+        print("No documents in the collection")
+
+
+except Exception as e:
+    print(e)
+finally:
+    client.close()
